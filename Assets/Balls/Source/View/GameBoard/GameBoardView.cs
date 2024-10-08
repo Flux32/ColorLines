8fc56d5666e587866cd400406d48329977d0308a
askobenko <88793343+Flux32@users.noreply.github.com>
2024-10-08 23:27:43 +0500

    Move GameBoardInputAction to new file

--- Assets/Balls/Source/View/GameBoard/GameBoardView.cs
+++ Assets/Balls/Source/View/GameBoard/GameBoardView.cs
@@ -194,12 +194,3 @@ namespace Balls.Source.View.GameBoard
         }
     }
 }
-
-public enum GameBoardInputAction : uint
-{
-    None = 0,
-    Hold = 1,
-    Press = 2,
-    CancelPress = 3,
-    Performed = 4,
-}
